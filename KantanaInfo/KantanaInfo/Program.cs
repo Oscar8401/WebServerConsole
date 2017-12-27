d306841a7ed46a5cb7408bf4529f36d37e7c6750
Taleb01 <taleb_n1@hotmail.com>
2017-12-27 11:41:45 +0100

    Adding a userWelcomePage

---
+++ KantanaInfo/KantanaInfo/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+namespace KatanaInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost:8080";
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stopping!");
+            }
+        }
+    }
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+
+            //app.Run(ctx =>
+            //{
+            //   return ctx.Response.WriteAsync("Hello World!");
+            //}
+            //);
+        }
+    }
+}
