bd24b7d744454006edfc9ecc27f52df4fce7f0a8
Taleb01 <taleb_n1@hotmail.com>
2017-12-27 12:31:59 +0100

    Middleware

--- KantanaInfo/KantanaInfo/Program.cs
+++ KantanaInfo/KantanaInfo/Program.cs
@@ -30,30 +30,81 @@ namespace KatanaInfo
 
             //app.Use<HelloWorldComponent>();
 
-            app.UseWelcomePage();
+            //app.UseWelcomePage();
 
             //app.Run(ctx =>
             //{
             //   return ctx.Response.WriteAsync("Hello World!");
             //}
             //);
+
+            app.Use(async(environment, next) => {
+                foreach (var pair in environment.Environment) { }
+                {
+                    Console.WriteLine("{0}:{1}", pair.Key, pair.Value);
+                }
+                await next();
+            });
+
+            app.Use(async (environment, next) => 
+            {
+                Console.WriteLine("Requesting : " + environment.Request.Path);
+                await next();
+                Console.WriteLine("Response : "+ environment.Response.StatusCode);
+            });
+            app.UseHelloWorld();
+        }
+    }
+
+    public static class AppBuilderExtentions
+    {
+        public static void UseHelloWorld(this IAppBuilder app)
+        {
+            app.Use<HelloWorldComponent>();
+        }
+    }
+        public static class AppBuilderExtensions
+    {
+        public static void UseHelloWorld(this IAppBuilder app)
+        {
+            app.Use<HelloWorldComponent>();
         }
     }
     //public class HelloWorldComponent
     //{
     //    AppFunc _next;
-    //    public HelloWorldComponent(AppFunc next)
+    //    public HelloWorldComponent (AppFunc next)
     //    {
     //        _next = next;
     //    }
-    //    public  Task Invoke(IDictionary<string, object> environment)
+    //    public Task Invoke(IDictionary<string, object> environment)
     //    {
-    //        //await _next(environment);
-    //        var response = environment["owin.ResponseBody"] as Stream;
+    //        var response = environment["owin.ResponseBoby"] as Stream;
     //        using (var writer = new StreamWriter(response))
     //        {
-    //            return writer.WriteLineAsync("Hello!");
+    //            return writer.WriteAsync("Hello!");
     //        }
     //    }
     //}
+
+
+
+
+    public class HelloWorldComponent
+    {
+        AppFunc _next;
+        public HelloWorldComponent(AppFunc next)
+        {
+            _next = next;
+        }
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            //await _next(environment);
+            var response = environment["owin.ResponseBody"] as Stream;
+            using (var writer = new StreamWriter(response))
+            {
+                return writer.WriteLineAsync("Hello!");
+            }
+        }
+    }
 }
